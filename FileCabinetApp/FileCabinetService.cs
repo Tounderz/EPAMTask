fac5376119687cac50f93a57bdd481c3e84ba5e7
Tounderz <85989034+Tounderz@users.noreply.github.com>
2021-11-26 23:01:04 +0300

    Add guard clauses to CreateRecord method.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -13,6 +13,32 @@ namespace FileCabinetApp
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short age, decimal salary, char symbol)
         {
             // TODO: добавьте реализацию метода
+            if (firstName.Length < 2 || firstName.Length > 60 || firstName.Contains(" "))
+            {
+                throw new ArgumentException("The size is from 2 to 60 characters and there should be no spaces", nameof(firstName));
+            }
+
+            if (string.IsNullOrEmpty(firstName))
+            {
+                throw new ArgumentNullException(nameof(firstName), "Can't be null");
+            }
+
+            if (lastName.Length < 2 || lastName.Length > 60 || lastName.Contains(" "))
+            {
+                throw new ArgumentException("The size is from 2 to 60 characters and there should be no spaces", nameof(lastName));
+            }
+
+            if (string.IsNullOrEmpty(lastName))
+            {
+                throw new ArgumentNullException(nameof(lastName), "Can't be null");
+            }
+
+            DateTime minDate = new DateTime(1950, 01, 01);
+            if (dateOfBirth > DateTime.Now || dateOfBirth < minDate)
+            {
+                throw new ArgumentException("Minimum date of birth 01/01/1950, and maximum - DateTime.Now", nameof(dateOfBirth));
+            }
+
             var record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
