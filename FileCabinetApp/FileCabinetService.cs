4fa81fa2f2e99679d9fb09fa4b9d3d167c261cb6
Tounderz <85989034+Tounderz@users.noreply.github.com>
2021-11-30 16:44:26 +0300

    Merge step4-add-search branch: Add implementation for find firstname command. Add implementation for find lastname command. Add implementation for find dateofbirth command. Improve find firstname performance with dictionary. Improve find lastname performance with dictionary. Improve find dateofbirth performance with dictionary.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 #pragma warning disable IDE0090
+#pragma warning disable CA1304
+#pragma warning disable CA1062
+#pragma warning disable CA1305
+#pragma warning disable IDE0060
 
 namespace FileCabinetApp
 {
     public class FileCabinetService
     {
         private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
+        public readonly Dictionary<string, List<FileCabinetRecord>> firstNameDictionary = new Dictionary<string, List<FileCabinetRecord>>();
+        private readonly Dictionary<string, List<FileCabinetRecord>> lastNameDictionary = new Dictionary<string, List<FileCabinetRecord>>();
+        private readonly Dictionary<string, List<FileCabinetRecord>> dateOfBirthDictionary = new Dictionary<string, List<FileCabinetRecord>>();
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short age, decimal salary, char symbol)
         {
@@ -45,18 +53,12 @@ namespace FileCabinetApp
                 throw new ArgumentException("The size of the string character is equal to one element", nameof(symbol));
             }
 
-            var record = new FileCabinetRecord
-            {
-                Id = this.list.Count + 1,
-                FirstName = firstName,
-                LastName = lastName,
-                DateOfBirth = dateOfBirth,
-                Age = age,
-                Salary = salary,
-                Symbol = symbol,
-            };
+            var record = AddRecord(this.list.Count + 1, firstName, lastName, dateOfBirth, age, salary, symbol);
 
             this.list.Add(record);
+            AddDitionary(firstName, record, this.firstNameDictionary);
+            AddDitionary(lastName, record, this.lastNameDictionary);
+            AddDitionary(dateOfBirth.ToString("yyyy-MMM-dd"), record, this.dateOfBirthDictionary);
 
             return record.Id;
         }
@@ -75,13 +77,79 @@ namespace FileCabinetApp
 
         public void EditRecord(int id, string firstName, string lastName, DateTime dateOfBirth, short age, decimal salary, char symbol)
         {
-            if (id > list.Count || id < 1)
+            if (id > this.list.Count || id < 1)
             {
                 throw new ArgumentException(null, nameof(id));
             }
-            else
+
+            var record = AddRecord(id, firstName, lastName, dateOfBirth, age, salary, symbol);
+            RemoveDitionary(id, this.firstNameDictionary);
+            AddDitionary(firstName, record, this.firstNameDictionary);
+            RemoveDitionary(id, this.lastNameDictionary);
+            AddDitionary(lastName, record, this.lastNameDictionary);
+            RemoveDitionary(id, this.dateOfBirthDictionary);
+            AddDitionary(dateOfBirth.ToString("yyyy-MMM-dd"), record, this.dateOfBirthDictionary);
+            this.list[id - 1] = record;
+        }
+
+        public FileCabinetRecord[] FindByFirstName(string firstName)
+        {
+            List<FileCabinetRecord> first = this.firstNameDictionary[firstName.ToUpper()];
+            return first.ToArray();
+        }
+
+        public FileCabinetRecord[] FindByLastName(string lastname)
+        {
+            List<FileCabinetRecord> first = this.lastNameDictionary[lastname.ToUpper()];
+            return first.ToArray();
+        }
+
+        public FileCabinetRecord[] FindByDateOfBirth(string date)
+        {
+            DateTime dateOfBirth = Convert.ToDateTime(date);
+            List<FileCabinetRecord> first = this.dateOfBirthDictionary[dateOfBirth.ToString("yyyy-MMM-dd")];
+            return first.ToArray();
+        }
+
+        private static FileCabinetRecord AddRecord(int id, string firstName, string lastName, DateTime dateOfBirth, short age, decimal salary, char symbol)
+        {
+            var record = new FileCabinetRecord
             {
-                GetRecords();
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+                DateOfBirth = dateOfBirth,
+                Age = age,
+                Salary = salary,
+                Symbol = symbol,
+            };
+
+            return record;
+        }
+
+        private static void AddDitionary(string key, FileCabinetRecord record, Dictionary<string, List<FileCabinetRecord>> dictionary)
+        {
+            var keyStr = key.ToUpper(CultureInfo.InvariantCulture);
+            if (!dictionary.ContainsKey(keyStr))
+            {
+                dictionary.Add(keyStr, new List<FileCabinetRecord>());
+            }
+
+            dictionary[keyStr].Add(record);
+        }
+
+        private static void RemoveDitionary(int id, Dictionary<string, List<FileCabinetRecord>> dictionary)
+        {
+            foreach (var item in dictionary)
+            {
+                foreach (var el in item.Value)
+                {
+                    if (el.Id == id)
+                    {
+                        item.Value.Remove(el);
+                        break;
+                    }
+                }
             }
         }
     }
