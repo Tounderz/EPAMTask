50f86552f452fee757bce03c25edbcf3d945e9f7
Tounderz <85989034+Tounderz@users.noreply.github.com>
2021-12-25 15:59:48 +0300

    Add storage command line parameter.

---
+++ FileCabinetApp/FileCabinetFilesystemService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#pragma warning disable SA1600
+
+namespace FileCabinetApp
+{
+    public class FileCabinetFilesystemService : IFileCabinetService
+    {
+        public int CreateRecord(FileCabinetRecord record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditRecord(int id, FileCabinetRecord record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<FileCabinetRecord> GetRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetRecordsCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<FileCabinetRecord> FindByDateOfBirth(string dateOfBirth)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<FileCabinetRecord> FindByFirstName(string firstName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<FileCabinetRecord> FindByLastName(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
