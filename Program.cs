d1b79e4fabfefd2fff4d02ecabb003194121f591
Tounderz <85989034+Tounderz@users.noreply.github.com>
2022-01-13 10:12:11 +0300

    Import csv and xml. New console application FileCabinetGenerator.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EPAMTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
